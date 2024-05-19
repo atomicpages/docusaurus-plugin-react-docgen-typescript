53950c011769af3a5661f126526c88d9d71061c4
atomicpages <djtthompson@gmail.com>
2024-05-19 13:25:27 -0700

    feat: move to tsup for transpiling to support ESM

--- scripts/files.sh
+++ scripts/files.sh
@@ -1,1 +1,6 @@
-find src -name "*.ts" -o -name "*.tsx"
+#!/usr/bin/env bash
+set -e
+
+readonly SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
+cd $SCRIPT_DIR/..
+pnpm glob 'src/**/*.{ts,tsx}'
